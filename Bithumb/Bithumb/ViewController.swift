7ffc8fa5137a6aab6fb91a9c3276dd37154f3eff
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-17 15:29:38 +0900

    [#1] 프로젝트 최초 생성

---
+++ Bithumb/Bithumb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bithumb
+//
+//  Created by Seungjin Baek on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
