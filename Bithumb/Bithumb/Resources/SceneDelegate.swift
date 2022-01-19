00736266c7cd0fbd6f4c44f1288ca7735af7a566
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-19 12:24:17 +0900

    [#3] 앱 시작시 진입 화면 설정

--- Bithumb/Bithumb/Resources/SceneDelegate.swift
+++ Bithumb/Bithumb/Resources/SceneDelegate.swift
@@ -14,6 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    self.window = UIWindow(windowScene: windowScene)
+    self.window?.rootViewController = TabBarController()
+    self.window?.makeKeyAndVisible()
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
