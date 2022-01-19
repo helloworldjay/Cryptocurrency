c8aff92938ad57df5aa0a02be205fb530bf10ba9
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-19 12:21:32 +0900

    [#3] Tab Bar 선택시 출력될 화면 ViewController 구현

---
+++ Bithumb/Bithumb/TBD/CurrentAsset/CurrentAssetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentAssetViewController.swift
+//  Bithumb
+//
+//  Created by Seungjin Baek on 2022/01/19.
+//
+
+import UIKit
+
+final class CurrentAssetViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
