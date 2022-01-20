ce21c4dadda65f9251e561261236989330b243e8
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-20 19:59:14 +0900

    [#5] CoinListView 구조 뼈대 구현

---
+++ Bithumb/Bithumb/Exchange/Components/CoinList/CoinListViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CoinListViewCell.swift
+//  Bithumb
+//
+//  Created by Seungjin Baek on 2022/01/20.
+//
+
+import UIKit
+
+final class CoinListViewCell: UITableViewCell {
+  
+}
---
+++ Bithumb/Bithumb/Exchange/Components/CoinList/CoinListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CoinListViewModel.swift
+//  Bithumb
+//
+//  Created by Seungjin Baek on 2022/01/20.
+//
+
+import Foundation
+
+struct CoinListViewModel {
+  
+}
