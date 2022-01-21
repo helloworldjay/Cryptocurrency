3b1019fb40e1af87b641f63859878dbcb7b8cdf0
babywalnut <anfuza@naver.com>
2022-01-21 19:35:54 +0900

    [#8] BarButtonItem을 ExchangeSearchBar 내부에 구현

--- Bithumb/Bithumb/Exchange/Components/ExchangeSearchBar/ExchangeSearchBar.swift
+++ Bithumb/Bithumb/Exchange/Components/ExchangeSearchBar/ExchangeSearchBar.swift
@@ -7,29 +7,77 @@
 
 import UIKit
 
+import SnapKit
 import Then
 
 final class ExchangeSearchBar: UISearchBar {
+
+  //MARK:  Properties
+  
+  private let giftButton = UIButton().then {
+    $0.setImage(UIImage(systemName: "gift"), for: .normal)
+    $0.tintColor = .black
+  }
+  
+  private let bellButton = UIButton().then {
+    $0.setImage(UIImage(systemName: "bell"), for: .normal)
+    $0.tintColor = .black
+  }
+  
+  
+  // MARK: Initializers
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    attribute()
+
+    configure()
+    layout()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  private func attribute() {
+  private func layout() {
+    self.addSubview(giftButton)
+    self.addSubview(bellButton)
+    
+    self.searchTextField.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(12)
+      $0.trailing.equalTo(giftButton.snp.leading).offset(-12)
+      $0.centerY.equalToSuperview()
+    }
+    
+    self.giftButton.snp.makeConstraints {
+      $0.trailing.equalTo(bellButton.snp.leading).offset(-12)
+      $0.centerY.equalToSuperview()
+    }
+    
+    self.bellButton.snp.makeConstraints {
+      $0.trailing.equalToSuperview().offset(-12)
+      $0.centerY.equalToSuperview()
+    }
+  }
+  
+  private func configure() {
     self.searchTextField.do {
       $0.layer.borderWidth = 2
-      $0.layer.borderColor = UIColor(red: 227/255, green: 129/255, blue: 30/255, alpha: 1).cgColor
+      $0.layer.borderColor = UIColor(
+        red: 227/255,
+        green: 129/255,
+        blue: 30/255,
+        alpha: 1
+      ).cgColor
       $0.layer.cornerRadius = 10
       $0.leftView?.tintColor = .black
       $0.backgroundColor = .systemBackground
       $0.placeholder = "코인명 또는 심볼 검색"
-      $0.tintColor = UIColor(red: 227/255, green: 129/255, blue: 30/255, alpha: 1)
+      $0.tintColor = UIColor(
+        red: 227/255,
+        green: 129/255,
+        blue: 30/255,
+        alpha: 1
+      )
     }
   }
-  
 }
--- Bithumb/Bithumb/Exchange/ExchangeViewController.swift
+++ Bithumb/Bithumb/Exchange/ExchangeViewController.swift
@@ -7,49 +7,9 @@
 
 import UIKit
 
-import Then
-
 final class ExchangeViewController: UIViewController {
-  
-  // MARK: Properties
-  
-  private let exchangeSearchBar = ExchangeSearchBar()
-  
-  private let giftButton = UIButton(frame: CGRect(
-     x: 0,
-     y: 0,
-     width: 30,
-     height: 20
-   )).then {
-     let giftImage = UIImage(systemName: "gift")
-     $0.setImage(giftImage, for: .normal)
-     $0.tintColor = .black
-   }
-  
-   private let bellButton = UIButton(frame: CGRect(
-     x: 0,
-     y: 0,
-     width: 30,
-     height: 20
-   )).then {
-     let bellImage = UIImage(systemName: "bell")
-     $0.setImage(bellImage, for: .normal)
-     $0.tintColor = .black
-   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    layout()
-  }
-  
-  private func layout() {
-    self.navigationItem.do {
-      $0.titleView = self.exchangeSearchBar
-      $0.rightBarButtonItems = [
-        UIBarButtonItem(customView: self.bellButton),
-        UIBarButtonItem(customView: self.giftButton)
-      ]
-    }
   }
 }
