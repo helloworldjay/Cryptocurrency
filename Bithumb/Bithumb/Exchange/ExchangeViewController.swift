db8b907aad0133dcb2ac8957f4c3ee91138dbd7e
babywalnut <anfuza@naver.com>
2022-01-21 12:47:38 +0900

    [#8] navigationBarButtonItem 구현

--- Bithumb/Bithumb/Exchange/ExchangeViewController.swift
+++ Bithumb/Bithumb/Exchange/ExchangeViewController.swift
@@ -13,7 +13,29 @@ final class ExchangeViewController: UIViewController {
   
   // MARK: Properties
   
-  let exchangeSearchBar = ExchangeSearchBar()
+  private let exchangeSearchBar = ExchangeSearchBar()
+  
+  private let giftButton = UIButton(frame: CGRect(
+     x: 0,
+     y: 0,
+     width: 30,
+     height: 20
+   )).then {
+     let giftImage = UIImage(systemName: "gift")
+     $0.setImage(giftImage, for: .normal)
+     $0.tintColor = .black
+   }
+  
+   private let bellButton = UIButton(frame: CGRect(
+     x: 0,
+     y: 0,
+     width: 30,
+     height: 20
+   )).then {
+     let bellImage = UIImage(systemName: "bell")
+     $0.setImage(bellImage, for: .normal)
+     $0.tintColor = .black
+   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,7 +46,10 @@ final class ExchangeViewController: UIViewController {
   private func layout() {
     self.navigationItem.do {
       $0.titleView = self.exchangeSearchBar
+      $0.rightBarButtonItems = [
+        UIBarButtonItem(customView: self.bellButton),
+        UIBarButtonItem(customView: self.giftButton)
+      ]
     }
   }
-  
 }
