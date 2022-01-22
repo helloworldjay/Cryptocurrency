ef0bbeb2e6274097b301be44fb5ac0d6fc472978
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-22 13:14:00 +0900

    [#9] 거래소 SearchBar의 ViewModel을 구현

--- Bithumb/Bithumb/Exchange/Components/ExchangeSearchBar/ExchangeSearchBarViewModel.swift
+++ Bithumb/Bithumb/Exchange/Components/ExchangeSearchBar/ExchangeSearchBarViewModel.swift
@@ -5,8 +5,24 @@
 //  Created by Seungjin Baek on 2022/01/20.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
 
-struct ExchangeSearchBarViewModel {
+final class ExchangeSearchBarViewModel {
   
+  // MARK: Properties
+  
+  let inputText = PublishRelay<String>()
+  let searchButtonTapped = PublishRelay<Void>()
+  let queryText: Observable<String>
+  
+  
+  // MARK: Initializers
+  
+  init() {
+    self.queryText = self.searchButtonTapped
+      .withLatestFrom(self.inputText)
+      .filter { !$0.isEmpty }
+      .distinctUntilChanged()
+  }
 }
