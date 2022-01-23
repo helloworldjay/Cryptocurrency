7d47b764ed8ff543070a1563f05c707bc3b1875f
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-23 16:49:59 +0900

    [#14] NetworkRequestRouter 구현

---
+++ Bithumb/Bithumb/Network/NetworkRequestRouter.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkRequestRouter.swift
+//  Bithumb
+//
+//  Created by Seungjin Baek on 2022/01/22.
+//
+
+import Foundation
+
+import Alamofire
+
+enum NetworkRequestRouter: URLRequestConvertible {
+  
+  case fetchTickerData(OrderCurrency, PaymentCurrency)
+  
+  private var baseURLString: String {
+    return "https://api.bithumb.com/public"
+  }
+  
+  private var HTTPMethod: Alamofire.HTTPMethod {
+    switch self {
+    case .fetchTickerData(_, _):
+      return .get
+    }
+  }
+  
+  private var path: String {
+    switch self {
+    case .fetchTickerData(let orderCurrency, let paymentCurrency):
+      return "\(self.baseURLString)" + "/ticker" + "/\(orderCurrency.rawValue)_\(paymentCurrency.rawValue)"
+    }
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let url = try self.path.asURL()
+    var request = URLRequest(url: url)
+    request.httpMethod = self.HTTPMethod.rawValue
+    
+    switch self {
+    case .fetchTickerData(_, _):
+      return request
+    }
+  }
+}
