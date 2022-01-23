066f91313c8578be30472fd75129ec23ecf2899f
helloworldjay <sj.seungjin.baek@gmail.com>
2022-01-23 17:01:18 +0900

    [#14] NetworkManager에서 데이터 전처리를 위한 기능 구현

--- Bithumb/Bithumb/Network/NetworkManager.swift
+++ Bithumb/Bithumb/Network/NetworkManager.swift
@@ -7,6 +7,37 @@
 
 import Foundation
 
-struct NetworkManager {
+import Alamofire
+import RxSwift
+
+struct NetworkManager: NetworkManagerLogic {
   
+  // MARK: Converters
+  
+  func dataToDecode(orderCurrency: OrderCurrency, data: Data?) -> Data? {
+    guard let data = data else { return nil }
+    if orderCurrency == .all {
+      guard let dateRemovedData = self.removeDate(from: data) else { return nil }
+      return dateRemovedData
+    }
+    return data
+  }
+  
+  private func removeDate(from data: Data) -> Data? {
+    if var data = self.convertToDictionary(from: data),
+       var tickerData = data["data"] as? [String: Any],
+       let _ = tickerData.removeValue(forKey: "date") {
+      data["data"] = tickerData
+      return self.convertToData(from: data)
+    }
+    return nil
+  }
+  
+  private func convertToDictionary(from data: Data) -> [String: Any]? {
+    return try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+  }
+  
+  private func convertToData(from dictionary: [String: Any]) -> Data? {
+    return try? JSONSerialization.data(withJSONObject: dictionary, options: [])
+  }
 }
