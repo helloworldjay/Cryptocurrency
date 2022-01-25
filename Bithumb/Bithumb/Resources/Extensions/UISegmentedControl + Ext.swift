742f2a677fbc46a55993ba21ef537558dee0697c
babywalnut <anfuza@naver.com>
2022-01-25 20:39:24 +0900

    [#15] segmentedController divdierColor수정을 위한 로직 갱신

--- Bithumb/Bithumb/Resources/Extensions/UISegmentedControl + Ext.swift
+++ Bithumb/Bithumb/Resources/Extensions/UISegmentedControl + Ext.swift
@@ -10,21 +10,10 @@ import UIKit
 extension UISegmentedControl {
   func setDividerColor(with color: UIColor) {
     setDividerImage(
-      imageWithColor(color: color),
+      color.image(CGSize(width: 0.1, height: 0.1)),
       forLeftSegmentState: .normal,
       rightSegmentState: .normal,
       barMetrics: .default
     )
-  }
-
-  private func imageWithColor(color: UIColor) -> UIImage {
-    let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
-    UIGraphicsBeginImageContext(rect.size)
-    let context = UIGraphicsGetCurrentContext()
-    context!.setFillColor(color.cgColor)
-    context!.fill(rect)
-    let image = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    return image!
   }
 }
