41e746019f18f5ef9b7afc52668ede220b87dd57
babywalnut <anfuza@naver.com>
2022-02-20 20:22:22 +0900

    [#42] FavoriteGridView 구현

---
+++ Cryptocurrency/Cryptocurrency/Exchange/Components/FavoriteGridView/FavoriteGridView.swift
@@ -0,0 +1,41 @@
+//
+//  FavoriteGridView.swift
+//  Cryptocurrency
+//
+//  Created by 김민성 on 2022/02/17.
+//
+
+import RxCocoa
+import RxSwift
+
+class FavoriteGridView: UICollectionView {
+
+  // MARK: Properties
+
+  private let disposeBag = DisposeBag()
+
+  // MARK: Initializers
+
+  init() {
+    let customFlowLayout = UICollectionViewFlowLayout()
+    customFlowLayout.scrollDirection = .vertical
+    let size = (UIScreen.main.bounds.size.width - CGFloat(80)) / CGFloat(2)
+    customFlowLayout.itemSize = CGSize(width: size, height: size)
+    customFlowLayout.minimumLineSpacing = 25
+
+    super.init(frame: .zero, collectionViewLayout: customFlowLayout)
+
+    attribute()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func attribute() {
+    self.backgroundColor = .white
+    self.contentInset = UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 25)
+    self.register(FavoriteGridViewCell.self, forCellWithReuseIdentifier: "FavoriteGridViewCell")
+
+  }
+}
