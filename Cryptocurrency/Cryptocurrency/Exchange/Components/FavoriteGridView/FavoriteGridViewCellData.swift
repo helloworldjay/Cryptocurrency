2d7c25ee0b5814a630da077c84b4cbe7a1a5d89f
babywalnut <anfuza@naver.com>
2022-02-20 20:07:57 +0900

    [#42] FavoriteGridViewCellData 구현

---
+++ Cryptocurrency/Cryptocurrency/Exchange/Components/FavoriteGridView/FavoriteGridViewCellData.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteGridViewCellData.swift
+//  Cryptocurrency
+//
+//  Created by 김민성 on 2022/02/16.
+//
+
+import Foundation
+
+struct FavoriteGridViewCellData {
+  let coinName: String
+  let ticker: String
+  let paymentCurrency: String
+}
