27266ab326534aef8679a82ac05fc4b27b462d69
babywalnut <anfuza@naver.com>
2022-02-20 20:51:11 +0900

    [#42] CoinDetailViewController에 구현될 관록 버튼의 enum타입 구현

---
+++ Cryptocurrency/Cryptocurrency/CoinDetail/CoinDetailFavoriteButtonState.swift
@@ -0,0 +1,13 @@
+//
+//  CoinDetailFavoriteButtonState.swift
+//  Cryptocurrency
+//
+//  Created by 김민성 on 2022/02/20.
+//
+
+import Foundation
+
+enum CoinDetailFavoriteButtonState: String {
+ case favorite = "star.fill"
+ case release = "star"
+}
