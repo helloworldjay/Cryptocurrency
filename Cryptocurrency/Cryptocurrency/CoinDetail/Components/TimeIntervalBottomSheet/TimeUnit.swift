8f48e44f16f8d2040e02168e57809a79e1ebc2d3
helloworldjay <sj.seungjin.baek@gmail.com>
2022-02-27 17:21:56 +0900

    [#72] TimeUnit rawValue를 프로퍼티 expression으로 변경

--- Cryptocurrency/Cryptocurrency/CoinDetail/Components/TimeIntervalBottomSheet/TimeUnit.swift
+++ Cryptocurrency/Cryptocurrency/CoinDetail/Components/TimeIntervalBottomSheet/TimeUnit.swift
@@ -7,14 +7,37 @@
 
 import Foundation
 
-enum TimeUnit: String, CaseIterable {
-  case oneMinute = "1m"
-  case threeMinute = "3m"
-  case fiveMinute = "5m"
-  case tenMinute = "10m"
-  case thirtyMinute = "30m"
-  case oneHour = "1h"
-  case sixHour = "6h"
-  case twelveHour = "12h"
-  case twentyFourHour = "24h"
+enum TimeUnit: CaseIterable {
+  case oneMinute
+  case threeMinute
+  case fiveMinute
+  case tenMinute
+  case thirtyMinute
+  case oneHour
+  case sixHour
+  case twelveHour
+  case twentyFourHour
+
+  var expression: String {
+    switch self {
+    case .oneMinute:
+      return "1m"
+    case .threeMinute:
+      return "3m"
+    case .fiveMinute:
+      return "5m"
+    case .tenMinute:
+      return "10m"
+    case .thirtyMinute:
+      return "30m"
+    case .oneHour:
+      return "1h"
+    case .sixHour:
+      return "6h"
+    case .twelveHour:
+      return "12h"
+    case .twentyFourHour:
+      return "24h"
+    }
+  }
 }
