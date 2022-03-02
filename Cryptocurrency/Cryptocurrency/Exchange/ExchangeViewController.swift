eadc0d1c5123829d654ba1429b9b6f57240a8e92
helloworldjay <sj.seungjin.baek@gmail.com>
2022-03-02 21:31:52 +0900

    [#72] ExchangeViewController 로직 위치 재조정 및 불필요한 공백 제거

--- Cryptocurrency/Cryptocurrency/Exchange/ExchangeViewController.swift
+++ Cryptocurrency/Cryptocurrency/Exchange/ExchangeViewController.swift
@@ -8,68 +8,68 @@
 import UIKit
 
 final class ExchangeViewController: UIViewController {
-  
+
   // MARK: Properties
-  
-  let coinListView: CoinListView
-  let exchangeSegmentedCategoryView: ExchangeSegmentedCategoryView
-  let exchangeSearchBar: ExchangeSearchBar
+
   var exchangeViewModel: ExchangeViewModelLogic
-  let exchangeUseCase: ExchangeUseCaseLogic
-  let networkManager: NetworkManagerLogic
+  private let networkManager: NetworkManagerLogic
+  private let exchangeUseCase: ExchangeUseCaseLogic
+  private let exchangeSearchBar: ExchangeSearchBar
+  private let exchangeSegmentedCategoryView: ExchangeSegmentedCategoryView
+  private let coinListView: CoinListView
 
 
   // MARK: Initializers
-  
+
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    self.exchangeSegmentedCategoryView = ExchangeSegmentedCategoryView()
     self.networkManager = NetworkManager()
     self.exchangeUseCase = ExchangeUseCase(network: self.networkManager)
     self.exchangeViewModel = ExchangeViewModel(useCase: self.exchangeUseCase)
     self.exchangeSearchBar = ExchangeSearchBar()
+    self.exchangeSegmentedCategoryView = ExchangeSegmentedCategoryView()
     self.coinListView = CoinListView()
-    
+
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
   }
-  
+
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     self.configure()
     self.layout()
   }
-  
+
   private func configure() {
     self.bind()
   }
-  
+
   private func bind() {
     self.exchangeSearchBar.bind(viewModel: self.exchangeViewModel.exchangeSearchBarViewModel)
-    self.coinListView.bind(viewModel: self.exchangeViewModel.coinListViewModel)
-    self.coinListView.coinListSortView.bind(coinListSortViewModel: self.exchangeViewModel.coinListSortViewModel)
     self.exchangeSegmentedCategoryView.bind(viewModel: self.exchangeViewModel.exchangeSegmentedCategoryViewModel)
+    self.coinListView.coinListSortView.bind(coinListSortViewModel: self.exchangeViewModel.coinListSortViewModel)
+    self.coinListView.bind(viewModel: self.exchangeViewModel.coinListViewModel)
   }
-  
+
   private func layout() {
     self.setUpNavigationBar()
-    
+
     [self.exchangeSegmentedCategoryView, self.coinListView].forEach { self.view.addSubview($0) }
-    
+
     self.exchangeSegmentedCategoryView.snp.makeConstraints {
       $0.leading.top.equalTo(self.view.safeAreaLayoutGuide).inset(10)
     }
-    
+
     self.coinListView.snp.makeConstraints {
       $0.top.equalTo(self.exchangeSegmentedCategoryView.snp.bottom).offset(10)
       $0.leading.trailing.equalToSuperview()
       $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
     }
   }
-  
+
   private func setUpNavigationBar() {
     self.navigationItem.titleView = exchangeSearchBar
   }
