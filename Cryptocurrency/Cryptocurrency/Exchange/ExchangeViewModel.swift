ae1edc3c19b698d527eebad47ab208dc4875cad1
helloworldjay <sj.seungjin.baek@gmail.com>
2022-03-02 22:44:38 +0900

    [#72] ExchangeViewModel 초기화 로직 분리

--- Cryptocurrency/Cryptocurrency/Exchange/ExchangeViewModel.swift
+++ Cryptocurrency/Cryptocurrency/Exchange/ExchangeViewModel.swift
@@ -17,68 +17,39 @@ protocol ExchangeViewModelLogic {
 }
 
 final class ExchangeViewModel: ExchangeViewModelLogic {
-  
+
   // MARK: Properties
-  
+
   let exchangeSearchBarViewModel: ExchangeSearchBarViewModelLogic
-  let coinListViewModel: CoinListViewModelLogic
-  let coinListSortViewModel: CoinListSortViewModelLogic
   let exchangeSegmentedCategoryViewModel: ExchangeSegmentedCategoryViewModelLogic
+  let coinListSortViewModel: CoinListSortViewModelLogic
+  let coinListViewModel: CoinListViewModelLogic
   var exchangeCoordinator: ExchangeCoordinator?
   private let orderCurrency = BehaviorSubject<OrderCurrency>(value: .all)
+  private let exchangeUseCase: ExchangeUseCaseLogic
   private let disposeBag = DisposeBag()
   
   
   // MARK: Initializers
-  
+
   init(useCase: ExchangeUseCaseLogic) {
     self.exchangeSearchBarViewModel = ExchangeSearchBarViewModel()
-    self.coinListViewModel = CoinListViewModel()
     self.coinListSortViewModel = CoinListSortViewModel()
     self.exchangeSegmentedCategoryViewModel = ExchangeSegmentedCategoryViewModel()
+    self.coinListViewModel = CoinListViewModel()
+    self.exchangeUseCase = useCase
 
-    let result = Observable.combineLatest(
-      self.orderCurrency,
-      self.exchangeSegmentedCategoryViewModel.paymentCurrency,
-      self.coinListSortViewModel.coinListSortCriteria
-    ) { orderCurrency, paymentCurrency, _ in
-      useCase.fetchTicker(orderCurrency: orderCurrency, paymentCurrency: paymentCurrency) }
-      .flatMap { $0 }
+    self.bind()
+    self.transition()
+  }
 
-    let cellData = result
-      .map(useCase.tickerResponse)
-      .filter { $0 != nil }
-      .map(useCase.coinListCellData)
-
-    let sortedCellData = Observable.combineLatest(
-      cellData,
-      self.coinListSortViewModel.coinListSortCriteria
-    ) { cellData, coinListSortCriteria -> [CoinListViewCellData] in
-      switch coinListSortCriteria.coinListSortCriterion {
-      case .coinName:
-        return useCase.sortByCoinName(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
-      case .currentPrice:
-        return useCase.sortByCurrentPrice(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
-      case .priceChangedRatio:
-        return useCase.sortByPriceChangedRatio(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
-      case .transactionAmount:
-        return useCase.sortByTransactionAmount(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
-      }
-    }
-
-    let filteredCellData = Observable.combineLatest(
-      sortedCellData,
-      self.exchangeSearchBarViewModel.orderCurrenciesToSearch
-    ) { sortedCellData, filteredOrderCurrencies in
-      return sortedCellData.filter { cellDatum in
-        filteredOrderCurrencies.values.contains(cellDatum.ticker)
-      }
-    }
-
-    filteredCellData
+  private func bind() {
+    self.filteredCellData()
       .bind(to: coinListViewModel.coinListCellData)
-      .disposed(by: disposeBag)
+      .disposed(by: self.disposeBag)
+  }
 
+  private func transition() {
     self.coinListViewModel.selectedOrderCurrency
       .subscribe(onNext: { orderCurrency in
         guard let paymentCurrency = try? self.exchangeSegmentedCategoryViewModel.paymentCurrency.value() else {
@@ -87,5 +58,50 @@ final class ExchangeViewModel: ExchangeViewModelLogic {
         self.exchangeCoordinator?.presentCoinDetailViewController(orderCurrency: orderCurrency,
                                                                   paymentCurrency: paymentCurrency)
       }).disposed(by: self.disposeBag)
+  }
+
+
+  // MARK: Streams
+
+  private func filteredCellData() -> Observable<[CoinListViewCellData]> {
+    return Observable.combineLatest(
+      self.sortedCellData(),
+      self.exchangeSearchBarViewModel.orderCurrenciesToSearch
+    ) { sortedCellData, filteredOrderCurrencies in
+      return sortedCellData.filter { cellDatum in
+        filteredOrderCurrencies.values.contains(cellDatum.ticker)
+      }
+    }
+  }
+
+  private func sortedCellData() -> Observable<[CoinListViewCellData]> {
+    return Observable.combineLatest(
+      self.coinListCellData(),
+      self.coinListSortViewModel.coinListSortCriteria
+    ) { cellData, coinListSortCriteria -> [CoinListViewCellData] in
+      switch coinListSortCriteria.coinListSortCriterion {
+      case .coinName:
+        return self.exchangeUseCase.sortByCoinName(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
+      case .currentPrice:
+        return self.exchangeUseCase.sortByCurrentPrice(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
+      case .priceChangedRatio:
+        return self.exchangeUseCase.sortByPriceChangedRatio(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
+      case .transactionAmount:
+        return self.exchangeUseCase.sortByTransactionAmount(coinListCellData: cellData, isDescending: coinListSortCriteria.isDescending)
+      }
+    }
+  }
+
+  private func coinListCellData() -> Observable<[CoinListViewCellData]> {
+    return Observable.combineLatest(
+      self.orderCurrency,
+      self.exchangeSegmentedCategoryViewModel.paymentCurrency,
+      self.coinListSortViewModel.coinListSortCriteria
+    ) { orderCurrency, paymentCurrency, _ in
+      self.exchangeUseCase.fetchTicker(orderCurrency: orderCurrency, paymentCurrency: paymentCurrency)
+    }.flatMap { $0 }
+    .map(self.exchangeUseCase.tickerResponse)
+    .filter { $0 != nil }
+    .map(self.exchangeUseCase.coinListCellData)
   }
 }
